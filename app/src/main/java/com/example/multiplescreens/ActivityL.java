c745fbb116c3957505ef80c564e849dd1addbea2
Adam Bloodgood <abloodg1@binghamton.edu>
2021-02-24 10:34:21 -0500

    Recreated project after it was lost

---
+++ app/src/main/java/com/example/multiplescreens/ActivityL.java
@@ -0,0 +1,32 @@
+package com.example.multiplescreens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ActivityL extends AppCompatActivity {
+
+    Button backButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_l);
+        backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchBack();
+            }
+        });
+    }
+
+    void switchBack(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
--- app/src/main/java/com/example/multiplescreens/MainActivity.java
+++ app/src/main/java/com/example/multiplescreens/MainActivity.java
@@ -2,13 +2,47 @@ package com.example.multiplescreens;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button leftButton;
+    Button rightButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        leftButton = findViewById(R.id.leftButton);
+        rightButton = findViewById(R.id.rightButton);
+
+        leftButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                switchLeft();
+            }
+        });
+        rightButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                switchRight();
+            }
+        });
     }
+
+    void switchLeft(){
+        Intent intent = new Intent(this, ActivityL.class);
+        startActivity(intent);
+    }
+
+    void switchRight(){
+        Intent intent = new Intent(this, ActivityR.class);
+        startActivity(intent);
+    }
+
 }
